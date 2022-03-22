658c93e24504cb7a332214f4aa7cb68dbfb986e8
TreeSoft <93043006+SoftOn1@users.noreply.github.com>
2022-03-22 20:11:43 +0200

    Add files via upload

---
+++ utils/set_bot_commands.py
@@ -0,0 +1,8 @@
+from aiogram import types
+
+
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands([
+        types.BotCommand("start", "💙 Запустить бота 💙")
+    ])
+#на этот раз мало говнокода
